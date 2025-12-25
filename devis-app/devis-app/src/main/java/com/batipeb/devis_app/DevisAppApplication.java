46e7f722ecd0f1fcaa54f6598ac36cb19adeb45f
Souhaib El Hassouni Amyay <souhaibhassouni0002@gmail.com>
2025-12-26 00:19:33 +0100

    Initial push

---
+++ devis-app/devis-app/src/main/java/com/batipeb/devis_app/DevisAppApplication.java
@@ -0,0 +1,13 @@
+package com.batipeb.devis_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevisAppApplication.class, args);
+	}
+
+}
---
+++ devis-app/devis-app/src/test/java/com/batipeb/devis_app/DevisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batipeb.devis_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
